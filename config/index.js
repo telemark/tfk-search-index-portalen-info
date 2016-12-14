af9c8d4d1837b2fffc7ac0f8e2f9bbcc07f36960
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-12-14 08:26:16 +0100

    Small refactoring

--- config/index.js
+++ config/index.js
@@ -1,11 +1,9 @@
 'use strict'
 
-var config = {
+module.exports = {
   JWT_KEY: process.env.JWT_KEY || 'Louie Louie, oh no, I got to go',
   SEARCH_SERVICE_URL: process.env.SEARCH_SERVICE_URL || 'https://search.service.com/api',
   SEARCH_SERVICE_INDEX: process.env.SEARCH_SERVICE_INDEX || 'portaleninfo',
   SEARCH_SERVICE_INDEX_TYPE: process.env.SEARCH_SERVICE_INDEX_TYPE || 'article',
   SOURCE_URL: process.env.SOURCE_URL || 'http://www.portalen.com/artikler.json'
 }
-
-module.exports = config
